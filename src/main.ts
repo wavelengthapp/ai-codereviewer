1df2e772a3d6ac64c3c7fea182320720abbbe8b8
vijaim <vijaianand@gmail.com>
2023-12-18 15:21:40 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -27,6 +27,7 @@ async function getPRDetails(): Promise<PRDetails> {
   const { repository, number } = JSON.parse(
     readFileSync(process.env.GITHUB_EVENT_PATH || "", "utf8")
   );
+  console.log(GITHUB_TOKEN, OPENAI_API_KEY, OPENAI_API_MODEL);
   const prResponse = await octokit.pulls.get({
     owner: repository.owner.login,
     repo: repository.name,
